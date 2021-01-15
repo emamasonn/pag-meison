9f46b9905318861a255757b18c86685330587ee9
David Mason <emamasonn@gmail.com>
2021-01-15 18:24:09 -0300

    update project

--- pages/post/[slug].js
+++ pages/post/[slug].js
@@ -2,9 +2,20 @@ import groq from "groq";
 import imageUrlBuilder from "@sanity/image-url";
 import BlockContent from "@sanity/block-content-to-react";
 import { Prism as SyntaxHighlighter } from "react-syntax-highlighter";
-import { coy } from "react-syntax-highlighter/dist/cjs/styles/prism";
+import { base16AteliersulphurpoolLight } from "react-syntax-highlighter/dist/cjs/styles/prism";
 import client from "../../client";
-import { Flex, Box, Image, Heading, Text, Button } from "@chakra-ui/react";
+import {
+  Flex,
+  Box,
+  Image,
+  Heading,
+  Text,
+  Button,
+  Menu,
+  MenuList,
+  MenuItem,
+  MenuButton,
+} from "@chakra-ui/react";
 import { Layout } from "../../components";
 import ReactMarkdown from "react-markdown";
 import styled from "@emotion/styled";
@@ -19,29 +30,86 @@ const Post = (props) => {
   const renderers = {
     code: ({ language, value }) => {
       return (
-        <SyntaxHighlighter
-          style={coy}
-          language={language}
-          children={value}
-          showLineNumbers={true}
-        />
+        <Box my="2rem">
+          <SyntaxHighlighter
+            style={base16AteliersulphurpoolLight}
+            language={language}
+            children={value}
+            showLineNumbers={true}
+          />
+        </Box>
       );
     },
-    heading: ({ children }) => <Heading fontSize="2.5rem">{children}</Heading>,
+    heading: ({ children }) => (
+      <Heading
+        fontSize="2rem"
+        mb="1rem"
+        mt="2rem"
+        letterSpacing="0rem"
+        fontWeight="600"
+      >
+        {children}
+      </Heading>
+    ),
+    paragraph: ({ children }) => (
+      <Text fontSize="1.1rem" letterSpacing="-0.003em" lineHeight="1.7rem">
+        {children}
+      </Text>
+    ),
   };
 
   return (
     <Layout>
-      <Box>
-        <Flex justifyContent="center" my="2rem">
-          <Heading>{title}</Heading>
+      <Flex maxH="20rem">
+        <Image src="/imagepost.jpg" objectFit="cover" />
+      </Flex>
+      <Box maxW="50rem" m="auto">
+        <Flex justifyContent="flex-start" mb="2rem" mt="3rem">
+          <Heading
+            w="full"
+            maxW="44rem"
+            fontSize={["2rem", "2rem", "3rem", "3rem"]}
+            letterSpacing="-0.011em"
+          >
+            {title}
+          </Heading>
         </Flex>
-        <Text fontSize="1.5rem">{synopsis}</Text>
-        <Flex mx={["0", "1rem", "5rem", "5rem"]} flexDirection="column">
+        <Flex justifyContent="flex-end" alignItems="center">
+          <Menu>
+            <MenuButton as={Button} bg="#fff">
+              <Image src="/share.svg" w="1.5rem" h="1.5rem" />
+            </MenuButton>
+            <MenuList minW="8rem">
+              <MenuItem display="flex" justifyContent="flex-start">
+                <Image
+                  src="/twitterLine.svg"
+                  w="1.5rem"
+                  h="1.5rem"
+                  mr="0.6rem"
+                />
+                <Text>Twitter</Text>
+              </MenuItem>
+              <MenuItem display="flex" justifyContent="flex-start">
+                <Image
+                  src="/linkedinLine.svg"
+                  w="1.5rem"
+                  h="1.5rem"
+                  mr="0.6rem"
+                />
+                <Text>Linkedin</Text>
+              </MenuItem>
+              <MenuItem display="flex" justifyContent="flex-start">
+                <Image src="/copylink.svg" w="1.5rem" h="1.5rem" mr="0.6rem" />
+                <Text>Copy Link</Text>
+              </MenuItem>
+            </MenuList>
+          </Menu>
+        </Flex>
+        <Flex flexDirection="column">
           <ReactMarkdown
             renderers={renderers}
             children={body}
-            escapeHtml={true}
+            escapeHtml={false}
           />
         </Flex>
         <Flex
